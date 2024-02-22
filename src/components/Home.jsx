587d1847b80527c5b861552949aaf2d7ca88ea1e
Barb T <stayyounng86@gmail.com>
2024-02-22 01:35:53 -0600

    home page complete

---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+import home_travel from '../assets/travel_home.jpg';
+
+function Home() {
+
+  return (
+    <secion>
+
+        <h1>Bienvenidos(Welcome)</h1>
+
+    <div className='homerow'>
+        <img src= {home_travel} />
+
+    <div className='homecolumn'>
+        
+        <h1>Global Wonders Travel Company</h1>
+        
+        <h3>Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Neque odio repudiandae 
+            culpa ullam, deserunt veritatis quaerat 
+            harum libero iste quod officiis possimus 
+            alias enim? Dignissimos dolores odio.
+            Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Nostrum, pariatur numquam
+            saepe alias dolorem tenetur aperiam est iusto
+            enim dolores? Vel quia laboriosam at animi
+            excepturi ab, ipsam est magni.
+        </h3>
+
+        <h2>Explore the GWTC Store</h2>
+
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing 
+            elit. Vero voluptatibus aliquam quidem aperia
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eaque voluptatibus blanditiis porro nihil voluptates.
+        </h3>
+
+    </div>
+    </div>
+
+    </secion>
+  )}
+export default Home
