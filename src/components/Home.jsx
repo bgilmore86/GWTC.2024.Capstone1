a6afcc4b6777f56e62e42acdaf93cb1fd2cf7c8e
Barb T <stayyounng86@gmail.com>
2024-03-06 18:12:26 -0600

    updated files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,10 +14,13 @@ function Home() {
     <div className='homerow'>
         <img className="travel_home"src= {travel} />
 
-    <div className='homecolumn'>
+    <div>
         
         <h1>Global Wonders Travel Company</h1>
-        <h3>Welcome to the Global Wonders Traveler App. Whether you are traveling solo or with family and friends, we can show you some major tourist attractions from around the world. Get started planning your next trip today! Visit our store to see our brilliant photograpy!
+        <h3>Welcome to the Global Wonders Traveler App. Whether you are traveling solo
+            or with family and friends, we can show you some major tourist attractions
+            from around the world. Get started planning your next trip today! Visit our
+            store to see our brilliant photograpy!
         </h3>
 
 
---
+++ src/components/NavbarB.jsx
@@ -0,0 +1,8 @@
+//showing navbar with other routes available
+export default function NavbarB() {
+    return (
+        <nav className="navbarB">
+            <a href="/account">Account</a>
+        </nav>
+    );
+}
